10761887f61dd68b1a2494e2ff26273091f8ffc6
Ariane <pikabooariane@hotmail.fr>
2021-02-15 14:23:00 +0200

    Added sendgrid tool

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 require 'faker'
 
+User.destroy_all
+
 10.times do
+  chosen_first_name = Faker::Name.first_name
+  chosen_last_name = Faker::Name.last_name
   User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
+    first_name: chosen_first_name,
+    last_name: chosen_last_name,
     description: "#{Faker::Job.title} in the #{Faker::Job.field} sector",
-    email: Faker::Internet.email
+    email: "#{chosen_first_name.downcase}@yopmail.com"
   )
 end
